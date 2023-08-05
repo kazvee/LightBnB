0e99b5d7ef229ec83393349dbfb39fbb7ba4a2a5
kazvee <kayvee123@gmail.com>
2023-08-05 15:56:43 -0600

    Refactor getUserWithEmail function to use data from the database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -2,6 +2,7 @@ const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const pool = new Pool({
@@ -21,15 +22,20 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && email && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+const getUserWithEmail = (email) => {
+  return pool
+    .query(
+      `SELECT * FROM users WHERE email = $1`, [email])
+    .then((result) => {
+      if (!result.rows.length) {
+        return null;
+      }
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
