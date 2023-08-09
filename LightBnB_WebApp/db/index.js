42d677df37544b3a9c45d4f166f3ce9729658afe
kazvee <kayvee123@gmail.com>
2023-08-09 12:23:01 -0600

    Remove unneeded query logging from index.js

--- LightBnB_WebApp/db/index.js
+++ LightBnB_WebApp/db/index.js
@@ -14,19 +14,8 @@ pool.connect().then(() => {
   console.log('Connected to the database! 😃');
 });
 
-// module.exports = {
-//   query: (text, params, callback) => {
-//     return pool.query(text, params, callback);
-//   }
-// };
-
-// Log every query executed, how long it took, and the number of rows it returned
 module.exports = {
   query: (text, params, callback) => {
-    const start = Date.now();
-    const res = pool.query(text, params, callback);
-    const duration = Date.now() - start;
-    console.log('Executed query! 🙂', { text, duration, rows: res.rowCount });
-    return res;
+    return pool.query(text, params, callback);
   }
 };
